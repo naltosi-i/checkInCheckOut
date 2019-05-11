760b81fe40ee5b4dfecd785d5db73978d876873d
磯村成利 <sol.naltosi@gmail.com>
2019-05-11 09:51:51 +0900

    Pythonファイルの更新

--- chInChOut.py
+++ chInChOut.py
@@ -1,6 +1,21 @@
 import ui
 
-def checkin:
+chkin = False
+
+def checkin():
+	if chkin == False:
+		
+		
+	else:
+		return 
+
+
+def checkout():
+	if chkin == True:
+		
+		
+	else:
+		return 
 
 v = ui.load_view()
 v.present('sheet')
