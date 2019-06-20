54a49bcd7a08aedf98e1255c426877e8d9d107cf
磯村成利 <sol.naltosi@gmail.com>
2019-06-20 12:52:28 +0900

    ClearConflict

--- chInChOut.py
+++ chInChOut.py
@@ -2,31 +2,31 @@ import ui, os
 from datetime import datetime
 
 
-def statusCheck(sender):
-	master_today = datetime.today()
-	master_str_today = f'{master_today:%Y-%m-%d}'
-	master_now = datetime.now()
-	master_str_now = f'{master_now:%Y-%m-%d %H:%M:%S}'
-	master_path = 'log/' + master_str_today + '.csv'
 
-	if os.path.exists(master_path):
-		with open(master_path, encoding='utf-8') as f:
-			for row in f:
-				lastRow = row.rstrip()
-			columns = lastRow.split(',')
-			lastTime = columns[0]
-			status = columns[1]
+master_today = datetime.today()
+master_str_today = f'{master_today:%Y-%m-%d}'
+master_now = datetime.now()
+master_str_now = f'{master_now:%Y-%m-%d %H:%M:%S}'
+master_path = 'log/' + master_str_today + '.csv'
+
+if os.path.exists(master_path):
+	with open(master_path, encoding='utf-8') as f:
+		for row in f:
+			lastRow = row.rstrip()
+		columns = lastRow.split(',')
+		lastTime = columns[0]
+		status = columns[1]
 	
-		label = sender.superview['label1']
-		text = 'last status\n' + status + 'at ' + lastTime
-		label.text = text
+	label = ui.Label('label1')
+	text = 'last status\n' + status + 'at ' + lastTime
+	label.text = text
 
 		if status == 'check_in':
 			chkin = False
 		elif status == 'check_out':
 			chkin = True
-	else:
-		chkin = False				
+else:
+	chkin = False				
 
 def checkin(sender):
 	global chkin
@@ -77,12 +77,12 @@ def checkout(sender):
 	else:
 		return
 
-def main():
-	statusCheck()
+#def main():
+#	statusCheck()
 	
 	
-if __name__ == '__main__':
-	main()
+#if __name__ == '__main__':
+#	main()
 	
 v = ui.load_view()
 v.present('sheet')
